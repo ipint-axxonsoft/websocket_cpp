0cb8104a544ad17822f5b60193804bb75229b57f
alim_abaev <alim.abaev@ru.axxonsoft.com>
2022-03-08 23:15:11 +0300

    Start Ws client impl. with tests

--- include/websocket_cpp/ws.hpp
+++ include/websocket_cpp/ws.hpp
@@ -1,6 +1,100 @@
 #pragma once
 
+#include <functional>
+
 namespace ws
 {
-	int foo(int a, int b) { return a + b; }
+	class DataMaskingHelper
+	{
+		const uint8_t* data_;
+		size_t data_len_ = 0;
+		unsigned key_;
+	public:
+		DataMaskingHelper(const uint8_t* data, size_t len, unsigned key)
+			: data_(data), data_len_(len), key_(key)
+		{
+		}
+
+		size_t Mask(char* out) {
+			auto* ptr = data_;
+			for (size_t i = 0; i < data_len_; ++i)
+			{
+				out[i] = ptr[i] ^ *((char*)(&key_) + i % 4 );
+			}
+
+			return data_len_;
+		}
+	};
+
+	class IWebSocket
+	{
+	public:
+		using DataReadyCallback_t = std::function<void(const char* data, size_t dataLen)>;
+		using DataWrapCallback_t = std::function<void(const char* data, size_t dataLen)>;
+
+	protected:
+		DataReadyCallback_t cb_;
+		DataWrapCallback_t wrapCb_;
+
+	public:
+		IWebSocket(DataReadyCallback_t cb, DataWrapCallback_t wrapCb)
+			: cb_(cb), wrapCb_(wrapCb)
+		{
+		}
+
+		virtual void SubmitChunk(const char* data, size_t dataLen) = 0;
+		virtual void WrapData(const char* data, size_t datalen) = 0;
+	};
+
+	class Client : public IWebSocket
+	{
+		enum { BUFFER_SIZE = 2048 };
+		char wrapBuffer[BUFFER_SIZE];
+	public:
+
+		Client(DataReadyCallback_t cb, DataWrapCallback_t wrapCb) : IWebSocket(cb, wrapCb)
+		{
+			memset(wrapBuffer, 0, BUFFER_SIZE);
+		}
+
+		// Inherited via IWebSocket
+		virtual void SubmitChunk(const char* data, size_t dataLen) override
+		{
+		}
+
+		virtual void WrapData(const char* data, size_t datalen) override
+		{
+			size_t resultWrapDataLen = 0;
+			wrapBuffer[0] |= 1U << 7; // set FIN bit
+			
+			wrapBuffer[0] |= 0x2; // set OP CODE. 0x2 is binary format
+			
+			wrapBuffer[1] |= 1U << 7; // set MASK flag
+
+			wrapBuffer[1] |= datalen; // set PAYLOAD LEN
+
+			uint32_t* maskingKey = (uint32_t*)(wrapBuffer + 2);
+			uint32_t someRandomMaskingKey = 0x19; // random 32-bit MASKING KEY, need to be generated randomly
+			*maskingKey = someRandomMaskingKey;
+
+			resultWrapDataLen = 6;
+
+			wrapCb_(wrapBuffer, resultWrapDataLen);
+		}
+	};
+
+	class Server : public IWebSocket
+	{
+	public:
+		Server(DataReadyCallback_t cb, DataWrapCallback_t wrapCb) : IWebSocket(cb, wrapCb) {}
+
+		// Inherited via IWebSocket
+		virtual void SubmitChunk(const char* data, size_t dataLen) override
+		{
+		}
+
+		virtual void WrapData(const char* data, size_t datalen) override
+		{
+		}
+	};
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,8 +1,53 @@
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
+
+#include <bitset>
+
 #include <websocket_cpp/ws.hpp>
 
-BOOST_AUTO_TEST_CASE(my_test)
+BOOST_AUTO_TEST_CASE(DataMaskingHelper_test0)
 {
-    BOOST_CHECK_EQUAL(ws::foo(1, 3), 4);
+    const size_t DATA_LEN = 5;
+    const char data[DATA_LEN] = "Test";
+    char outBuffer[DATA_LEN];
+    int key = 33;
+    auto resLen = ws::DataMaskingHelper((uint8_t*)data, 5, key).Mask(outBuffer);
+
+    BOOST_CHECK(resLen == DATA_LEN); // just check some MASKING KEY is set, should be 32 bit value
+}
+
+BOOST_AUTO_TEST_CASE(Client_test0)
+{
+    const size_t DATA_BUFFER_LEN = 2048;
+    char wrapDataBuffer[DATA_BUFFER_LEN];
+    char plainDataBuffer[DATA_BUFFER_LEN];
+
+    memset(wrapDataBuffer, 0, DATA_BUFFER_LEN);
+    memset(plainDataBuffer, 0, DATA_BUFFER_LEN);
+    size_t wrapDataLen = 0;
+
+    auto dataReadyCb = [plainDataBuffer](const char* data, size_t len) {};
+    auto wrapCb = [&wrapDataBuffer, &wrapDataLen](const char* wrappedData, size_t len) {
+        memcpy(wrapDataBuffer, wrappedData, len);
+		wrapDataLen = len;
+    };
+
+    auto c = new ws::Client(dataReadyCb, wrapCb);
+
+    char plainText[] = "Hello, World!";
+    size_t plainTextLen = strlen(plainText);
+    c->WrapData(plainText, plainTextLen);
+    BOOST_CHECK(std::bitset<8>(wrapDataBuffer[0]).test(7) == 1); // check FIN bit is 1
+    
+    BOOST_CHECK(std::bitset<4>(wrapDataBuffer[0]) == 0x2); // check OP code, we use only binary
+    
+    BOOST_CHECK(std::bitset<8>(wrapDataBuffer[1]).test(7) == 1); // check MASK code, for client it's always true
+    
+    BOOST_CHECK(std::bitset<7>(wrapDataBuffer[1]) == plainTextLen); // check payload len
+    
+    BOOST_CHECK(wrapDataLen == 6);
+
+    BOOST_CHECK(*((uint32_t*)wrapDataBuffer + 2) == 0x19); // just check some MASKING KEY is set, should be 32 bit value
+
+    delete c;
 }
